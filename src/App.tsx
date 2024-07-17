bd9eadfbeb7622ce5e9f71983e4ace3aeba458c0
hayasin2004 <masataka1kousuke1@gmail.com>
2024-07-17 22:47:49 +0900

    レクチャー数11までの内容 sidebarLeftのscss設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import './App.scss';
+import Sidebar from "./componetns/sidebar/Sidebar";
 
 function App() {
     return (
         <div className="App">
-            <h1>
-                discord
-            </h1>
+        {/*    Sidebar*/}
+            <Sidebar />
         </div>
     );
 }
---
+++ src/componetns/sidebar/Sidebar.tsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import "./Sidebar.scss"
+class Sidebar extends Component {
+    render() {
+        return (
+            <div className="sidebar">
+            {/*    SidebarRight*/}
+
+                <div className="sidebarLeft">
+
+                    <div className="serverIcon">
+                        <img src="/heart.png" alt="heart"/>
+                    </div>
+                    <div className="serverIcon">
+                        <img src="/heart.png" alt="heart"/>
+                    </div>
+
+                </div>
+
+                {/*    SidebarLeft*/}
+                <div className="sidebarRight">
+                    <div className="sidebarTop">
+                        <h3>
+                            Discord
+                        </h3>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
